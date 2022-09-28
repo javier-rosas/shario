9ab6d066ddb7aab641f6f2a3b7a11fff03e468e8
javier-rosas <jr2dj@virginia.edu>
2022-09-27 23:21:24 -0400

    added button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import SideBar from './SideBar.js'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
 
   return (
     <div className='App'>
       <h1>hello</h1>
-      <SideBar/>
-      <button className="button-main-page"> BOTON</button>
+      <Form>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Input</Form.Label>
+        <Form.Control type="text" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      <Button>hwllo</Button>
+      <Button variant="primary" className="button-main-page"> BOTON</Button>
     </div>
   );
 }
