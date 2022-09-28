7a9dd7b6040cf6a447ca7650225935a2d8bbc0cb
javier-rosas <jr2dj@virginia.edu>
2022-09-27 23:11:17 -0400

    created main div and SideBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import SideBar from './SideBar.js'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <div className="">  </div>
+    <div className='App'>
+      <h1>hello</h1>
+      <SideBar/>
+      <button className="button-main-page"> BOTON</button>
     </div>
   );
 }
---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+import './SideBar.css'
+
+function SideBar() {
+
+  return (
+    <div className="sidebar">
+      <h1> Hello </h1>
+    </div>
+  )
+}
+
+export default SideBar
