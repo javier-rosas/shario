afc6d1161ffb766d4dcc79b3eed39a056f666d80
manfredoli <pecchio@alumni.usc.edu>
2022-11-29 18:46:18 -0500

    Added python script to convert pdf to word

--- conversions/pdfToWord/PDFToWordConversionScript.py
+++ conversions/pdfToWord/PDFToWordConversionScript.py
@@ -1,11 +1,37 @@
-import PyPDF2
+# import PyPDF2
 
-FILE_PATH = 'sharioPDF.pdf'
+# FILE_PATH = 'sharioPDF.pdf'
 
-with open(FILE_PATH, mode='rb') as f:
+# with open(FILE_PATH, mode='rb') as f:
 
-    reader = PyPDF2.PdfFileReader(f)
+#     reader = PyPDF2.PdfFileReader(f)
 
-    page = reader.getPage(0)
+#     page = reader.getPage(0)
 
-    print(page.extractText())
+#     print(page.extractText())
+
+# PDF to Word Script
+
+import win32com.client
+import os
+
+# Connects to Word
+word=win32com.client.Dispatch("word.Application")
+word.visible=0
+
+# Grab pdf doc
+doc_pdf="test.pdf"
+input_file=os.path.abspath(doc_pdf)
+
+# Open input file
+wb=word.Documents.Open(input_file)
+
+# Create output PDF file
+output_file=os.path.abspath(doc_pdf[0:-4] + "docx".format())
+wb.SaveAs2(output_file, FileFormat=16)
+
+print("PDF to  DOCx is completed")
+wb.Close()
+
+#Exit Word
+word.Quit()
